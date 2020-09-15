30e1114d1a65c48c9e928505e13e9908c9526bcf
Ainun Septian <ainunseptian79@gmail.com>
2020-09-15 11:47:29 +0700

    melakukan perubahan kecil pada explore reman news menambahkan section berita terkini

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Image, ScrollView, TouchableOpacity } from 'react-native';
 import { RumahAktif, Chating, Live, Account, Zone, Cari, Scanner, EducationIcon, HealthIcon, EventsIcon, OtherIcon } from '../../assets';
 
 const Home = ({navigation}) => {
@@ -8,7 +8,7 @@ const Home = ({navigation}) => {
     };
     return (
         <View style={{flex: 1}}>
-            <View style={{flex: 1, backgroundColor: 'white'}}>
+            <ScrollView style={{flex: 1, backgroundColor: 'white'}}>
                 {/* Search Bar */}
                 <View style={{marginHorizontal: 17, flexDirection: 'row'}}>
                     <View style={{position: 'relative'}}>
@@ -25,30 +25,50 @@ const Home = ({navigation}) => {
                 <View style={{marginHorizontal: 17, marginTop: 20}}>
                     <View>
                         <View style={{backgroundColor: '#2c5fbb', borderTopLeftRadius: 6, borderTopRightRadius: 6, padding: 12}}>
-                            <Text style={{color: 'white', fontSize: 16, fontWeight: 'bold'}}>Explore Reman's News</Text>
+                            <Text style={{color: 'white', fontSize: 16, fontWeight: 'bold'}}>Explore REMAN NEWS</Text>
                         </View>
                     </View>
-                    <View style={{flexDirection: 'row', paddingTop: 15, paddingBottom: 14, backgroundColor: '#2f65bd'}}>
+                    <View style={{flexDirection: 'row', paddingTop: 20, paddingBottom: 18, backgroundColor: '#2f65bd'}}>
                         <View style={{flex: 1, alignItems: 'center'}}>
-                            <EducationIcon width={30} height={35} />
+                            <EducationIcon width={35} height={35} />
                             <Text style={{fontSize: 13, color: 'white'}}>Education</Text>
                         </View>
                         <View style={{flex: 1, alignItems: 'center'}}>
-                            <HealthIcon width={30} height={35} />
+                            <HealthIcon width={35} height={35} />
                             <Text style={{fontSize: 13, color: 'white'}}>Health</Text>
                         </View>
                         <View style={{flex: 1, alignItems: 'center'}}>
-                            <EventsIcon width={30} height={35} />
+                            <EventsIcon width={35} height={35} />
                             <Text style={{fontSize: 13, color: 'white'}}>Events</Text>
                         </View>
                         <View style={{flex: 1, alignItems: 'center'}}>
-                            <OtherIcon width={30} height={35} />
+                            <OtherIcon width={35} height={35} />
                             <Text style={{fontSize: 13, color: 'white'}}>Other</Text>
                         </View>
                     </View>
                 </View>
-                {/* Explore Reman's News */}
-            </View>
+                {/* AKhir Explore Reman's News */}
+
+                <View style={{height: 17, backgroundColor: '#f2f2f4', marginTop: 20}}></View>
+
+                {/* Berita Terkini Section */}
+                <View style={{paddingTop: 16, paddingHorizontal: 16}}>
+                    <Text style={{fontSize: 18, fontWeight: 'bold', alignSelf: 'center'}}>Berita Terkini</Text>
+                    <View style={{position: 'relative', paddingTop: 12}}>
+                        <Image source={require('../../assets/image/Succes.jpg')} style={{height: 170, width: '100%', borderRadius: 6}} />
+                        <View style={{width: '100%', height: '100%', position: 'absolute', top: 12, left: 0, backgroundColor: 'black', opacity: 0.13, borderRadius: 6}}></View>
+                    </View>
+                    <View style={{paddingTop: 16, paddingBottom: 20, borderBottomColor: '#e8e9ed', borderBottomWidth: 1, marginBottom: 20}}>
+                        <Text style={{fontSize: 16, fontWeight: 'bold', color: '#1c1c1c'}}>REMAN NEWS</Text>
+                        <Text style={{fontSize: 14, fontWeight: 'normal', color: '#7a7a7a', marginBottom: 12}}>New normal atau kebiasaan baru akan diterapkan mulai Agustus 2020 di Indonesia</Text>
+                        <TouchableOpacity style={{backgroundColor: '#1bc900', paddingHorizontal: 12, paddingVertical: 11, alignSelf: 'flex-end', borderRadius: 5}}>
+                            <Text style={{fontSize: 13, fontWeight: 'bold', color: 'white'}}>READ</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+                {/* Akhir Berita Terkini Section */}
+
+            </ScrollView>
             {/* Navigation */}
             <View style={{height: 56, flexDirection: 'row'}}>
                 <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
